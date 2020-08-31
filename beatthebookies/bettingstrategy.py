849295651d3977a3086e02031834627299c9e9eb
Chris Westerman <chris.westerman8@gmail.com>
2020-08-31 22:27:36 +0100

    tried predict proba with custom threshold

--- beatthebookies/bettingstrategy.py
+++ beatthebookies/bettingstrategy.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import sqlite3
-from utils import compute_overall_scores
 
 
 def simple_betting_profits(df, bet=10):
@@ -42,69 +41,24 @@ def simple_betting_profits(df, bet=10):
 
     return fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total
 
-def model_profits(df, bet=10):
-    pass
 
-def correct_single(x):
+def correct_bet(x):
     if x['pred'] == 1:
         if x['act'] == 1:
           return abs(x['bet'] * x['winning_odds']) + x['bet']
         return x['bet']
     return 0
 
-def correct_multi(x):
-    if x['diff'] == 0:
-        return abs(x['bet'] * x['winning_odds']) + x['bet']
-    return x['bet']
-
-def correct_threshold(x):
-    if x['pct'] > x['threshold']:
-        if x['act'] == 1:
-            return abs(x['bet'] * x['winning_odds']) + x['bet']
-        else:
-          return x['bet']
-    return 0
-
-def round_to(x):
-    if x['pct'] > 0.5:
-        return 1
-    return 0
 
 
-def compute_profit(df,y_pred,y_true,bet, y_type, threshold=0.5):
+def compute_profit(df,y_pred,y_true,bet):
     fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total = simple_betting_profits(df.copy(), bet=bet)
-    if y_type == 'single':
-        combined = pd.DataFrame({'pred': y_pred, 'act': y_true, 'winning_odds': df['winning_odds']})
-        combined['bet'] = -bet
-        # print('len', len(combined))
-        print('pred:',combined.pred.sum())
-        # print('act:',combined.act.sum())
-        combined['profit'] = combined.apply(lambda x: correct_single(x) , axis=1)
-        # print('profit:', combined.profit.sum())
-        btb_profit_total = combined['profit'].sum()
-        combined = combined.sort_values(by='profit', ascending=False)
-        # print(combined.head(50))
-        # print(combined.tail(50))
-        return btb_profit_total, fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total
-    if y_type == 'multi':
-        # code for converting 3 to 1
-        # y_pred = (y_pred == y_pred.max(axis=1)[:,None]).astype(int)
-        # variance = abs(y_pred - y_true)
-        # diff = np.sum(a=variance, axis=1)
-        # combined = pd.DataFrame({'diff': diff, 'winning_odds': df['winning_odds']})
-        # combined['bet'] = -bet
-        # combined['profit'] = combined.apply(lambda x: correct_multi(x) , axis=1)
-        # btb_profit_total = combined['profit'].sum()
-        # return btb_profit_total, fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total
-        combined = pd.DataFrame({'pct': y_pred, 'act': y_true, 'winning_odds': df['winning_odds']})
-        combined['bet'] = -bet
-        combined['threshold'] = threshold
-        combined['profit'] = combined.apply(lambda x: correct_threshold(x), axis=1)
-        combined['predict'] = combined.apply(lambda x: round_to(x), axis=1)
-        btb_profit_total = combined['profit'].sum()
-        scores = compute_overall_scores(y_true, combined.predict)
-        print(btb_profit_total)
-        return scores, btb_profit_total, fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total
+    combined = pd.DataFrame({'pred': y_pred, 'act': y_true, 'winning_odds': df['winning_odds']})
+    combined['bet'] = -bet
+    combined['profit'] = combined.apply(lambda x: correct_bet(x) , axis=1)
+    btb_profit_total = combined['profit'].sum()
+    combined = combined.sort_values(by='profit', ascending=False)
+    return btb_profit_total, fav_profit_total, dog_profit_total, home_profit_total, draw_profit_total, away_profit_total
 
 
 
